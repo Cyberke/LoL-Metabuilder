b4c294f719cfa670a9866a68a7b335de84e07222
Cyberke <jelle1vdb@hotmail.com>
2015-01-20 18:21:05 +0100

    Build saven werkt. opvragen builds ook
    Code van inlezen build staat op dropbox.
    Deze heeft het path van de file nodig, maar JSON inlezen code bestaat al
    werkend.

--- LoLMetaBuilder/app/src/main/java/be/howest/lolmetabuilder/BuildsFragment.java
+++ LoLMetaBuilder/app/src/main/java/be/howest/lolmetabuilder/BuildsFragment.java
@@ -2,16 +2,26 @@ package be.howest.lolmetabuilder;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class BuildsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    private ListView lstBuilds;
+    private ArrayList<File> files = new ArrayList<File>();
 
     public static BuildsFragment newInstance() {
         BuildsFragment fragment = new BuildsFragment();
@@ -44,7 +54,32 @@ public class BuildsFragment extends Fragment {
         actionBar.removeAllTabs();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
 
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File dir = new File(root.getAbsolutePath() + "/LoLMetaBuilder");
+        //files ophalen in folder
+        files = getDirectoryFiles(dir);
+
+        lstBuilds = (ListView) view.findViewById(R.id.lstBuilds);
+        lstBuilds.setAdapter(new BuildAdapter());
+
         return view;
+    }
+
+    private ArrayList<File> getDirectoryFiles(File dirPath)
+    {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = dirPath.listFiles();
+        for(File file : files)
+        {
+            if(!file.isDirectory())
+            {
+                if(file.getName().endsWith(".bld"))
+                {
+                    inFiles.add(file);
+                }
+            }
+        }
+        return inFiles;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -72,6 +107,57 @@ public class BuildsFragment extends Fragment {
 
     public interface OnFragmentInteractionListener {
         public void onFragmentInteraction(Uri uri);
+    }
+
+
+    class BuildAdapter extends ArrayAdapter<File>
+    {
+        public BuildAdapter(){
+            super(getActivity().getBaseContext(), R.layout.listitem_build, R.id.txtFilename);
+
+            this.addAll(files);
+        }
+
+        class ViewHolder {
+            TextView txtFilename;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //View row = super.getView(position, convertView, parent);
+            ViewHolder viewHolder = new ViewHolder();
+
+            final File file = files.get(position);
+
+            if(convertView == null)
+            {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                convertView = inflater.inflate(R.layout.listitem_build, null);
+                viewHolder = new ViewHolder();
+
+                viewHolder.txtFilename = (TextView) convertView.findViewById(R.id.txtFilename);
+
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.txtFilename.setText(file.getName());
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity().getBaseContext(), file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                    /*Intent intent = new Intent(getBaseContext(), Build_detail.class);
+                    intent.putExtra(Intent.EXTRA_TEXT, file.getAbsolutePath());
+
+                    startActivity(intent);*/
+                }
+            });
+
+            return convertView;
+        }
     }
 
 }
--- LoLMetaBuilder/app/src/main/java/be/howest/lolmetabuilder/GenerateBuildResultFragment.java
+++ LoLMetaBuilder/app/src/main/java/be/howest/lolmetabuilder/GenerateBuildResultFragment.java
@@ -2,12 +2,26 @@ package be.howest.lolmetabuilder;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class GenerateBuildResultFragment extends Fragment {
 
@@ -21,6 +35,24 @@ public class GenerateBuildResultFragment extends Fragment {
 
     public GenerateBuildResultFragment() {
         // Required empty public constructor
+    }
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.menu_my_generate_build_result, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId()){
+            case R.id.action_save:
+                showFileNamePrompt();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -42,7 +74,104 @@ public class GenerateBuildResultFragment extends Fragment {
         actionBar.removeAllTabs();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
 
+        setHasOptionsMenu(true);
+
         return view;
+    }
+
+    public void showFileNamePrompt(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity().getBaseContext());
+
+        alert.setTitle("Filename");
+        alert.setMessage("Please enter a name for the build");
+
+        // Set an EditText view to get user input
+        final EditText input = new EditText(getActivity().getBaseContext());
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString();
+                //Trim front & end spaces from the input
+                value = value.trim();
+                //replace " " with a "_"
+                value = value.replace(" ", "_");
+
+                writeFile(value);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
+    }
+
+    public void writeFile(String value){
+
+        if(isExternalStorageWritable())
+        {
+            File root = android.os.Environment.getExternalStorageDirectory();
+
+            File dir = new File(root.getAbsolutePath() + "/LoLMetaBuilder");
+            dir.mkdirs();
+
+            String fileName = MainActivity.championBuild.getChampion().getName() + "_" + value + ".bld";
+
+            File file = new File(dir, fileName);
+
+            //Toast.makeText(getBaseContext(), "" + file, Toast.LENGTH_LONG).show();
+
+            if(!file.exists())
+            {
+                try{
+                    FileOutputStream f = new FileOutputStream(file);
+                    PrintWriter pw = new PrintWriter(f);
+
+                    pw.println("{");
+                    pw.println("\"Build\":{");
+                    pw.println("\"Champion\":" + MainActivity.championBuild.getChampion().getId()+",");
+
+                    pw.println("\"Items\":[");
+                    for(int i=0; i< MainActivity.championBuild.getItems().length; i++)
+                    {
+                        if(i<5)
+                        {
+                            pw.println("{\"Item\":" + MainActivity.championBuild.getItems()[i].getId()+ "},");
+                        }else{
+                            pw.println("{\"Item\":" + MainActivity.championBuild.getItems()[i].getId() + "}");
+                        }
+                    }
+                    pw.println("]");
+                    pw.println("}");
+                    pw.println("}");
+
+                    pw.flush();
+                    pw.close();
+                    f.close();
+
+                    Toast.makeText(getActivity().getBaseContext(), "Build saved", Toast.LENGTH_LONG).show();
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                Toast.makeText(getActivity().getBaseContext(), "Filename already exists", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 
     public void onButtonPressed(Uri uri) {
